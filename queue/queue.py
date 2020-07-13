77e8909b1fbdab48de232dd6febcfa0c2fc51d7f
dylan0stewart <57077858+dylan0stewart@users.noreply.github.com>
2020-07-13 19:01:21 -0400

    assignment finished

--- queue/queue.py
+++ queue/queue.py
@@ -1,7 +1,7 @@
+from singly_linked_list import LinkedList
 """
 A queue is a data structure whose primary purpose is to store and
 return elements in First In First Out order. 
-
 1. Implement the Queue class using an array as the underlying storage structure.
    Make sure the Queue tests pass.
 2. Re-implement the Queue class, this time using the linked list implementation
@@ -13,16 +13,39 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+    
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def enqueue(self, value):
+#         self.storage.append(value)
+
+#     def dequeue(self):
+#         if self.storage:
+#             return self.storage.pop(0)
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
     
     def __len__(self):
-        pass
+        l = 0
+        if self.storage.head:
+            l = 1
+            curr = self.storage.head
+            while curr.get_next() is not None:
+                l += 1
+                curr = curr.get_next()
+        return l
 
     def enqueue(self, value):
-        pass
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
-        pass
+        if self.storage:
+            return self.storage.remove_head()
--- stack/stack.py
+++ stack/stack.py
@@ -1,7 +1,7 @@
+from singly_linked_list import LinkedList
 """
 A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order. 
-
 1. Implement the Stack class using an array as the underlying storage structure.
    Make sure the Stack tests pass.
 2. Re-implement the Stack class, this time using the linked list implementation
@@ -10,16 +10,41 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+# array method
+
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def push(self, value):
+#         self.storage.append(value)
+
+#     def pop(self):
+#         if self.storage:
+#             return self.storage.pop()
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
 
     def __len__(self):
-        pass
+        l = 0
+        if self.storage.head:
+            l = 1
+            curr = self.storage.head
+            while curr.get_next() is not None:
+                l += 1
+                curr = curr.get_next()
+        return l
 
     def push(self, value):
-        pass
+        self.storage.add_to_tail(value)
 
     def pop(self):
-        pass
+        if self.storage:
+            return self.storage.remove_head()
